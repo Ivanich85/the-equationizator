64adf4bd169d72f8359fe05ae8e9195020fab829
Ivanich85 <ivan.dmitriev85@gmail.com>
2019-03-24 00:40:03 +0300

    Логика проверки переписана через RegEx. Добавлена подсветка inputов

--- equationizator.js
+++ equationizator.js
@@ -1,89 +1,100 @@
+// Pattern for an equation parsing
+var EQUATION_PATTERN = /(\d+)(\+|\-)(\d+)=(\d+)/;
+
+// Equation`s parts
+var DIGIT_ONE;
+var SIGN;
+var DIGIT_TWO;
+var RESULT;
+
+var DIGIT_REQUIRED = 'Напиши число! 😐';
+var CORRECT_RESULT = 'Правильно! 😉';
+var INCORRECT_RESULT = 'Неправильно... 😰';
 
 window.onload = function () {
     checkResult();
 }
 
 function checkResult() {
-    var checkResultButton = document.querySelector('.check_button');
-    checkResultButton.addEventListener('click', getAllEquations);
+    document.querySelector('.check_button').addEventListener('click', checkAllEquations);
 }
 
-function getAllEquations() {
+function checkAllEquations() {
     Array.from(document.getElementsByClassName('equation')).forEach(function (item) {
-        parseEquation(item);
+        checkInputValue(item);
     });
 }
 
-function parseEquation(equation) {
-    var equationMembers = Array.from(equation.getElementsByClassName('member'));
-    var digitOne;
-    var sign;
-    var digitTwo;
-    var result;
-    if (equationMembers.length == 5) {
-        equationMembers.forEach(function (item, i) {
-            switch (i) {
-                case 0:
-                    digitOne = getValueFromEquationMember(item);
-                case 1:
-                    sign = getValueFromEquationMember(item);
-                case 2:
-                    digitTwo = getValueFromEquationMember(item);
-                // под номером 3 находится знак равенства. Он нам не нужен для разбора
-                case 4:
-                    result = getValueFromEquationMember(item);
-            }
-        });
-        isCalculateResultEqualsResult(sign, digitOne, digitTwo, result);
-    } else {
-        console.error('Неверный формат уравнения');
-    }
+function transformEquationDomToString(equation) {
+    var quationString = '';
+    Array.from(equation.getElementsByClassName('member')).forEach(function (item) {
+        quationString += getValueFromEquationMember(item);
+    });
+    return quationString;
 }
 
 function getValueFromEquationMember(equationMember) {
     var memberValue;
     if (equationMember.classList.contains('input_member')) {
-        if (isNumeric(equationMember.value)) { // Если поле input не заполнено, проставляем 0
-            memberValue = parseInt(equationMember.value);
-        } else {
-            memberValue = 0;
-        }
+        memberValue = parseInt(equationMember.value);
     } else {
-        memberValue = equationMember.innerHTML.trim();
+        memberValue = equationMember.innerHTML;
     }
     return memberValue;
+}
+
+function checkInputValue(item) {
+    var inputOfEquation = item.querySelector('.input_member');
+    var checkResultString = item.querySelector('.check_result');
+    if (isNumeric(inputOfEquation.value)) {
+        inputOfEquation.classList.remove('empty_value');
+        // вот здесь проверяем верный ответ или нет
+        checkResultString.innerHTML = 
+            isCalculateResultEqualsResult(transformEquationDomToString(item)) ? CORRECT_RESULT : INCORRECT_RESULT;
+    } else {
+        inputOfEquation.classList.add('empty_value');
+        checkResultString.innerHTML = DIGIT_REQUIRED;
+    }
 }
 
 function isNumeric(value) {
     return !isNaN(parseInt(value));
 }
 
-function isCalculateResultEqualsResult(sign, digitOne, digitTwo, result) {
-    console.log('calculation: ' + calculateResult(sign, digitOne, digitTwo)); // Отладка. Удалить
-    console.log('result: ' + result);
-    console.log('is equal: ' + (calculateResult(sign, digitOne, digitTwo) == result));
-    console.log('----------------------')
-    return calculateResult(sign, digitOne, digitTwo) == result;
-}
-
-function calculateResult(sign, digitOne, digitTwo) {
-    var calculationResult;
-    if (isAdditionDigits(sign)) {
-        calculationResult = additionDigits(digitOne, digitTwo)
+function isCalculateResultEqualsResult(equationString) {
+    if (checkEquationStringFormat(equationString)) {
+        parseEquationString(equationString);
+        return calculateResult() == RESULT;
     } else {
-        calculationResult = subtractionDigits(digitOne, digitTwo)
+        console.error('Неверный формат уравнения');
+        return false;
     }
-    return calculationResult;
 }
 
-function isAdditionDigits(sign) {
-    return sign == '+';
+function checkEquationStringFormat(equationString) {
+    return EQUATION_PATTERN.test(equationString);
 }
 
-function additionDigits(digitOne, digitTwo) {
-    return parseInt(digitOne) + parseInt(digitTwo);
+function parseEquationString(equationString) {
+    var equationArray = EQUATION_PATTERN.exec(equationString);
+    DIGIT_ONE = equationArray[1];
+    SIGN = equationArray[2];
+    DIGIT_TWO = equationArray[3];
+    RESULT = equationArray[4];
 }
 
-function subtractionDigits(digitOne, digitTwo) {
-    return parseInt(digitOne) - parseInt(digitTwo);
+function calculateResult() {
+    return isAdditionDigits() ? additionDigits() : subtractionDigits();
+}
+
+function isAdditionDigits() {
+    return SIGN == '+';
+}
+
+function additionDigits() {
+    return parseInt(DIGIT_ONE) + parseInt(DIGIT_TWO);
+}
+
+function subtractionDigits() {
+    return parseInt(DIGIT_ONE) - parseInt(DIGIT_TWO);
 }
