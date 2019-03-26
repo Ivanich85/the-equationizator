3dc24e9fb67e3f58e6ac4514c43e0602bb0fd02b
Ivanich85 <ivan.dmitriev85@gmail.com>
2019-03-26 23:47:38 +0300

    Добавлен генератор уравнений

--- equationizator.js
+++ equationizator.js
@@ -1,3 +1,6 @@
+var NUMBER_OF_EQUATIONS = 9;
+var MAXIMUM_DIGIT = 30;
+
 // Pattern for an equation parsing
 var EQUATION_PATTERN = /(\d+)(\+|\-)(\d+)=(\d+)/;
 
@@ -12,7 +15,101 @@ var CORRECT_RESULT = 'Правильно! 😉';
 var INCORRECT_RESULT = 'Неправильно... 😰';
 
 window.onload = function () {
+    addEquationToField();
     checkResult();
+}
+
+function addEquationToField() {
+    var field = document.querySelector('.equations_field');
+    for (var i = 0; i < NUMBER_OF_EQUATIONS - 3; i++) {
+        field.appendChild(createRandomEquationType());
+    }
+}
+
+// Equation member constructor
+function createMember(element, elemClass, value) {
+    var member = document.createElement(element);
+    member.classList.add('member');
+    member.classList.add(elemClass);
+    member.innerHTML = value;
+    return member;
+}
+
+function createDigit() {
+    return createMember('div', 'digit_member', createRandomDigit());
+}
+
+function createInput() {
+    var input = createMember('input', 'input_member', null);
+    input.maxLength = 3;
+    input.type = 'text';
+    return input;
+}
+
+function createSign() {
+    return createMember('div', 'sign_member', createRandomSign());
+}
+
+function createEqually() {
+    return createMember('div', 'equally_member', '=');
+}
+
+function createResult() {
+    return createMember('div', 'result_member', createRandomDigit());
+}
+
+function createCheckResult() {
+    var checkResult = createMember('div', 'check_result', '');
+    checkResult.classList.remove('member');
+    return checkResult;
+}
+
+function createRandomSign() {
+    return Math.random() > 0.5 ? '+' : '-';
+}
+
+function createRandomDigit() {
+    return Math.ceil(Math.random() * MAXIMUM_DIGIT);
+}
+
+// Equation constructor
+function createEquation(digitOrInputOne, sign, digitOrInputTwo, equally, digitOrInputThree, checkResult) {
+    var equation = document.createElement('div');
+    equation.classList.add('equation');
+    equation.appendChild(digitOrInputOne);
+    equation.appendChild(sign);
+    equation.appendChild(digitOrInputTwo);
+    equation.appendChild(equally);
+    equation.appendChild(digitOrInputThree);
+    equation.appendChild(checkResult);
+    return equation;
+}
+
+function createRandomEquationType() {
+    var randomEquation = Math.random();
+    if (randomEquation < 0.33) {
+        equation = createEquationTypeOne();
+    } else if (randomEquation > 0.66) {
+        equation = createEquationTypeThree();
+    } else {
+        equation = createEquationTypeTwo();
+    }
+    return equation;
+}
+
+function createEquationTypeOne() {
+    return createEquation(createDigit(), createSign(), createInput(),
+        createEqually(), createResult(), createCheckResult());
+}
+
+function createEquationTypeTwo() {
+    return createEquation(createDigit(), createSign(), createDigit(),
+        createEqually(), createInput(), createCheckResult());
+}
+
+function createEquationTypeThree() {
+    return createEquation(createInput(), createSign(), createDigit(),
+        createEqually(), createResult(), createCheckResult());
 }
 
 function checkResult() {
@@ -21,35 +118,16 @@ function checkResult() {
 
 function checkAllEquations() {
     Array.from(document.getElementsByClassName('equation')).forEach(function (item) {
-        checkInputValue(item);
+        isAnswerCorrect(item);
     });
 }
 
-function transformEquationDomToString(equation) {
-    var quationString = '';
-    Array.from(equation.getElementsByClassName('member')).forEach(function (item) {
-        quationString += getValueFromEquationMember(item);
-    });
-    return quationString;
-}
-
-function getValueFromEquationMember(equationMember) {
-    var memberValue;
-    if (equationMember.classList.contains('input_member')) {
-        memberValue = parseInt(equationMember.value);
-    } else {
-        memberValue = equationMember.innerHTML;
-    }
-    return memberValue;
-}
-
-function checkInputValue(item) {
+function isAnswerCorrect(item) {
     var inputOfEquation = item.querySelector('.input_member');
     var checkResultString = item.querySelector('.check_result');
     if (isNumeric(inputOfEquation.value)) {
         inputOfEquation.classList.remove('empty_value');
-        // вот здесь проверяем верный ответ или нет
-        checkResultString.innerHTML = 
+        checkResultString.innerHTML =
             isCalculateResultEqualsResult(transformEquationDomToString(item)) ? CORRECT_RESULT : INCORRECT_RESULT;
     } else {
         inputOfEquation.classList.add('empty_value');
@@ -69,6 +147,24 @@ function isCalculateResultEqualsResult(equationString) {
         console.error('Неверный формат уравнения');
         return false;
     }
+}
+
+function transformEquationDomToString(equation) {
+    var quationString = '';
+    Array.from(equation.getElementsByClassName('member')).forEach(function (item) {
+        quationString += getValueFromEquationMember(item);
+    });
+    return quationString;
+}
+
+function getValueFromEquationMember(equationMember) {
+    var memberValue;
+    if (equationMember.classList.contains('input_member')) {
+        memberValue = parseInt(equationMember.value);
+    } else {
+        memberValue = equationMember.innerHTML;
+    }
+    return memberValue;
 }
 
 function checkEquationStringFormat(equationString) {
