a1075c562ec51ef178a3454af3c0d86783b6ec1c
Ivanich85 <ivan.dmitriev85@gmail.com>
2019-03-30 00:14:16 +0300

    Доработана логика. Добавлена оценка

--- equationizator.js
+++ equationizator.js
@@ -1,8 +1,18 @@
-var NUMBER_OF_EQUATIONS = 9;
-var MAXIMUM_DIGIT = 27;
+var NUMBER_OF_EQUATIONS = 3;
+
+var MAXIMUM_DIGIT = 22;
+var NUMBER_OF_TRIES = 3;
 
 // Pattern for an equation parsing
 var EQUATION_PATTERN = /(\d+)(\+|\-)(\d+)=(\d+)/;
+
+// Check result string classes
+var INCORRECT_ANSWER = 'incorrect_answer';
+var EMPTY_VALUE = 'empty_answer';
+var CORRECT_ANSWER = 'correct_answer';
+
+// log
+var INCORRECT_EQUATION_FORMAT = 'Неверный формат уравнения';
 
 // Equation`s parts
 var DIGIT_ONE;
@@ -15,20 +25,28 @@ var DIGIT_ONE_VALUE;
 var DIGIT_TWO_VALUE;
 var SIGN_VALUE;
 
+var SIGN_PLUS = '+';
+var SIGN_MINUS = '-';
+
+var BUTTON_TEXT = 'Проверить ответ. Осталось попыток: ';
+
 var WRITE_ANSWER = '😉';
 var DIGIT_REQUIRED = 'Напиши ответ! 😡';
 var CORRECT_RESULT = 'Правильно! 😍';
 var INCORRECT_RESULT = 'Неправильно... 😰';
 
-var SIGN_PLUS = '+';
-var SIGN_MINUS = '-';
-
-var NUMBER_OF_TRIES = 3;
-var BUTTON_TEXT = 'Проверить ответ. \nОсталось попыток: ';
+var GRADE_TWO = 'Плохо 2';
+var GRADE_THREE = 'Удовл. 3';
+var GRADE_FOUR = 'Хор. 4';
+var GRADE_FIVE = 'Отл. 5';
 
 window.onload = function () {
+    gameStart();
+}
+
+function gameStart() {
     addEquationToField();
-    checkResult();
+    activateResultButton();
 }
 
 function addEquationToField() {
@@ -36,6 +54,62 @@ function addEquationToField() {
     for (var i = 0; i < NUMBER_OF_EQUATIONS; i++) {
         field.appendChild(createRandomEquationType());
     }
+}
+
+function createRandomEquationType() {
+    generateDigitValuesAndSignValue();
+    var randomEquation = Math.random();
+    if (randomEquation < 0.33) {
+        equation = createEquationTypeOne();
+    } else if (randomEquation > 0.66) {
+        equation = createEquationTypeThree();
+    } else {
+        equation = createEquationTypeTwo();
+    }
+    return equation;
+}
+
+function generateDigitValuesAndSignValue() {
+    DIGIT_ONE_VALUE = createRandomDigit();
+    DIGIT_TWO_VALUE = createRandomDigit();
+    SIGN_VALUE = createRandomSign();
+    if (isEquationResultNegativeNumber()) {
+        generateDigitValuesAndSignValue();
+    }
+}
+
+function isEquationResultNegativeNumber() {
+    var signValueMinus = SIGN_VALUE == SIGN_PLUS && DIGIT_ONE_VALUE > DIGIT_TWO_VALUE;
+    var signValuePlus = SIGN_VALUE == SIGN_MINUS && DIGIT_ONE_VALUE < DIGIT_TWO_VALUE;
+    return signValueMinus || signValuePlus;
+}
+
+// Equation constructor
+function createEquation(digitOrInputOne, sign, digitOrInputTwo, equally, digitOrInputThree, checkResult) {
+    var equation = document.createElement('div');
+    equation.classList.add('equation');
+    equation.appendChild(digitOrInputOne);
+    equation.appendChild(sign);
+    equation.appendChild(digitOrInputTwo);
+    equation.appendChild(equally);
+    equation.appendChild(digitOrInputThree);
+    equation.appendChild(checkResult);
+    return equation;
+}
+
+function createEquationTypeOne() {
+    return createEquation(createDigit(DIGIT_ONE_VALUE), createSign(SIGN_VALUE), createInput(),
+        createEqually(), createResult(DIGIT_TWO_VALUE), createCheckResult());
+}
+
+function createEquationTypeTwo() {
+    return createEquation(createDigit(DIGIT_ONE_VALUE), createSign(SIGN_VALUE), createDigit(DIGIT_TWO_VALUE),
+        createEqually(), createInput(), createCheckResult());
+}
+
+function createEquationTypeThree() {
+    return createEquation(createInput(), createSign(SIGN_VALUE), createDigit(DIGIT_ONE_VALUE),
+        createEqually(), createResult(DIGIT_TWO_VALUE), createCheckResult());
 }
 
 // Equation member constructor
@@ -85,63 +159,7 @@ function createRandomDigit() {
     return Math.ceil(Math.random() * MAXIMUM_DIGIT);
 }
 
-// Equation constructor
-function createEquation(digitOrInputOne, sign, digitOrInputTwo, equally, digitOrInputThree, checkResult) {
-    var equation = document.createElement('div');
-    equation.classList.add('equation');
-    equation.appendChild(digitOrInputOne);
-    equation.appendChild(sign);
-    equation.appendChild(digitOrInputTwo);
-    equation.appendChild(equally);
-    equation.appendChild(digitOrInputThree);
-    equation.appendChild(checkResult);
-    return equation;
-}
-
-function generateDigitValuesAndSignValue() {
-    DIGIT_ONE_VALUE = createRandomDigit();
-    DIGIT_TWO_VALUE = createRandomDigit();
-    SIGN_VALUE = createRandomSign();
-    if (isEquationResultNegativeNumber()) {
-        generateDigitValuesAndSignValue();
-    }
-}
-
-function isEquationResultNegativeNumber() {
-    var signValueMinus = SIGN_VALUE == SIGN_PLUS && DIGIT_ONE_VALUE > DIGIT_TWO_VALUE;
-    var signValuePlus = SIGN_VALUE == SIGN_MINUS && DIGIT_ONE_VALUE < DIGIT_TWO_VALUE;
-    return signValueMinus || signValuePlus;
-}
-
-function createRandomEquationType() {
-    generateDigitValuesAndSignValue();
-    var randomEquation = Math.random();
-    if (randomEquation < 0.33) {
-        equation = createEquationTypeOne();
-    } else if (randomEquation > 0.66) {
-        equation = createEquationTypeThree();
-    } else {
-        equation = createEquationTypeTwo();
-    }
-    return equation;
-}
-
-function createEquationTypeOne() {
-    return createEquation(createDigit(DIGIT_ONE_VALUE), createSign(SIGN_VALUE), createInput(),
-        createEqually(), createResult(DIGIT_TWO_VALUE), createCheckResult());
-}
-
-function createEquationTypeTwo() {
-    return createEquation(createDigit(DIGIT_ONE_VALUE), createSign(SIGN_VALUE), createDigit(DIGIT_TWO_VALUE),
-        createEqually(), createInput(), createCheckResult());
-}
-
-function createEquationTypeThree() {
-    return createEquation(createInput(), createSign(SIGN_VALUE), createDigit(DIGIT_ONE_VALUE),
-        createEqually(), createResult(DIGIT_TWO_VALUE), createCheckResult());
-}
-
-function checkResult() {
+function activateResultButton() {
     var button = document.querySelector('.check_button');
     uploadButtonText(button);
     button.addEventListener('click', checkAllEquations);
@@ -151,23 +169,39 @@ function checkAllEquations() {
     Array.from(document.getElementsByClassName('equation')).forEach(function (item) {
         isAnswerCorrect(item);
     });
-    disabledButton(checkIsWin());
+    if (isWin() || isGameOver()) {
+        disabledButton();
+        getGrade();
+    }
 }
 
-function disabledButton(isWinOrLose) {
-    document.querySelector('.check_button').disabled = isWinOrLose;
+function disabledButton() {
+    document.querySelector('.check_button').disabled = true;
 }
 
-function checkIsWin() {
+function isWin() {
+    return Array.from(document.getElementsByClassName('equation correct_answer')).length == NUMBER_OF_EQUATIONS;
+}
+
+function isGameOver() {
     decreaseTries();
     uploadButtonText();
-    if (allResultsAreCorrect()) {
-        console.log("Молодец!");
-        return true;
-    } else if (NUMBER_OF_TRIES <= 0) {
-        console.log("Поражение");
-        return true;
+    return NUMBER_OF_TRIES <= 0;
+}
+
+function getGrade() {
+    var grade = document.querySelector('.grade');
+    var correctAnswersPercent = Array.from(document.getElementsByClassName('equation correct_answer')).length / NUMBER_OF_EQUATIONS;
+    if (correctAnswersPercent >= 0.9) {
+        grade.innerHTML = GRADE_FIVE;
+    } else if (correctAnswersPercent >= 0.75 && correctAnswersPercent < 0.9) {
+        grade.innerHTML = GRADE_FOUR;
+    } else if (correctAnswersPercent >= 0.6 && correctAnswersPercent < 0.75) {
+        grade.innerHTML = GRADE_THREE;
+    } else {
+        grade.innerHTML = GRADE_TWO;
     }
+    grade.classList.add('grade_animate');
 }
 
 function uploadButtonText() {
@@ -179,26 +213,15 @@ function decreaseTries() {
     NUMBER_OF_TRIES--;
 }
 
-function allResultsAreCorrect() {
-    var correctResultsNumber = 0;
-    Array.from(document.getElementsByClassName('check_result')).forEach(function (item) {
-        if (item.innerHTML == CORRECT_RESULT) {
-            correctResultsNumber++;
-            console.log(correctResultsNumber);
-        }
-    });
-    return correctResultsNumber == NUMBER_OF_EQUATIONS;
-}
-
 function isAnswerCorrect(item) {
     var inputOfEquation = item.querySelector('.input_member');
     var checkResultString = item.querySelector('.check_result');
     if (isNumeric(inputOfEquation.value)) {
-        inputOfEquation.classList.remove('empty_value');
-        checkResultString.innerHTML =
-            isCalculateResultEqualsResult(transformEquationDomToString(item)) ? CORRECT_RESULT : INCORRECT_RESULT;
+        inputOfEquation.classList.remove(EMPTY_VALUE);
+        createResultString(item, isCalculateResultEqualsResult(transformEquationDomToString(item)));
     } else {
-        inputOfEquation.classList.add('empty_value');
+        inputOfEquation.classList.add(EMPTY_VALUE);
+        removeCorrectOrIncorrectColors(item);
         checkResultString.innerHTML = DIGIT_REQUIRED;
     }
 }
@@ -212,7 +235,7 @@ function isCalculateResultEqualsResult(equationString) {
         parseEquationString(equationString);
         return calculateResult() == RESULT;
     } else {
-        console.error('Неверный формат уравнения');
+        console.error(INCORRECT_EQUATION_FORMAT);
         return false;
     }
 }
@@ -261,4 +284,39 @@ function additionDigits() {
 
 function subtractionDigits() {
     return parseInt(DIGIT_ONE) - parseInt(DIGIT_TWO);
+}
+
+function createResultString(item, resultCorrect) {
+    var checkResultString = item.querySelector('.check_result'); 
+    if (resultCorrect) {
+        checkResultString.innerHTML = CORRECT_RESULT;
+        coloredElemCorrect(item);
+    } else {
+        checkResultString.innerHTML = INCORRECT_RESULT;
+        coloredElemIncorrect(item);
+    }
+}
+
+function coloredElemCorrect(elem) {
+    var input = elem.querySelector('.input_member');
+    elem.classList.add(CORRECT_ANSWER);
+    elem.classList.remove(INCORRECT_ANSWER);
+    input.classList.add(CORRECT_ANSWER);
+    input.classList.remove(INCORRECT_ANSWER);
+}
+
+function coloredElemIncorrect(elem) {
+    var input = elem.querySelector('.input_member');
+    elem.classList.remove(CORRECT_ANSWER);
+    elem.classList.add(INCORRECT_ANSWER);
+    input.classList.remove(CORRECT_ANSWER);
+    input.classList.add(INCORRECT_ANSWER);
+}
+
+function removeCorrectOrIncorrectColors(elem) {
+    var input = elem.querySelector('.input_member');
+    elem.classList.remove(CORRECT_ANSWER);
+    elem.classList.remove(INCORRECT_ANSWER);
+    input.classList.remove(CORRECT_ANSWER);
+    input.classList.remove(INCORRECT_ANSWER);
 }
